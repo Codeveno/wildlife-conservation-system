da4588f63592b7f8374ef8b3b5d9ed119f14ecf3
codeveno <samsonochieng476@gmail.com>
2025-02-28 13:42:17 +0300

    STYLING

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,8 +1,12 @@
 // scripts.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Example: Update live video feed
+    //Update live video feed
     const liveFeed = document.getElementById("live-feed");
     if (liveFeed) {
         liveFeed.src = "{{ url_for('static', filename='images/live_feed.mp4') }}";
     }
+});
+document.addEventListener("DOMContentLoaded", function() {
+    let video = document.getElementById("background-video");
+    video.playbackRate = 0.5; 
 });
